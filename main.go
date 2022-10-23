58f5d969bb0676f1ad10c2064007eeab1f3c5d85
Vance12355 <haibiev.amir@gmail.com>
2022-10-23 15:47:46 +0300

    дописал хандлеры с gorm

--- main.go
+++ main.go
@@ -1,61 +1,136 @@
 package main
 
 import (
-	"Pears/avl"
-	"database/sql"
-	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	"github.com/lib/pq"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var users []User
+type User struct {
+	id    int
+	login string
+	mail  string
+}
+
+var DB *gorm.DB
 
 func main() {
-
-	r := mux.NewRouter()
-
 	// подключение к бд
-	connStr := "user=postgres password=mypas dbname=pears sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	//получение данных с бд users
-	rows, err := db.Query("SELECT * FROM users")
+	dsn := "host=localhost user=postgres password=mypas dbname=pears port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	tree := avl.New()
+	DB = db
 
-	defer rows.Close()
-	for rows.Next() {
-		row := user{}
-		err := rows.Scan(&row.id, &row.name, &row.mail)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(row.id, row.name, row.mail)
-		tree.Insert(row.name)
-	}
+	// создание роутера
+	r := mux.NewRouter()
 
 	r.HandleFunc("/users", getUsers).Methods("GET")
 	r.HandleFunc("/users/{id}", getUser).Methods("GET")
 	r.HandleFunc("/users", createUser).Methods("POST")
 	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
 	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", r))
-
+	http.ListenAndServe(":4000", r)
+	log.Println("API is running!")
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
+	// получение данных из бд из таблицы users
+	var users []User
+	DB.Find(&users)
+
+	// вывод данных в консоль
+	log.Println(users)
+
+	// возвращение ответа
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// чтение параметров запроса
+	params := mux.Vars(r)
 
+	// получение пользователя из бд
+	var user User
+	if err := DB.First(&user, params["id"]).Error; err != nil {
+		log.Fatalln(err)
+	}
+
+	// возвращение ответа
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	// чтение тела запроса
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// преобразование тела запроса в структуру
+	var user *User
+	json.Unmarshal(body, &user)
+
+	// добавление пользователя в бд если нет ошибок
+	if err := DB.Create(user).Error; err != nil {
+		log.Fatalln(err)
+	}
+
+	// возвращение ответа
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode("User created successfully")
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	// чтение параметров запроса
+	params := mux.Vars(r)
+
+	// чтение тела запроса
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// преобразование тела запроса в структуру
+	var user *User
+	json.Unmarshal(body, &user)
+
+	// обновление пользователя в бд если нет ошибок
+	if err := DB.Model(&User{}).Where("id = ?", params["id"]).Updates(user).Error; err != nil {
+		log.Fatalln(err)
+	}
+
+	// возвращение ответа
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("User updated successfully")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	// чтение параметров запроса
+	params := mux.Vars(r)
+
+	// удаление пользователя из бд
+	if err := DB.Delete(&User{}, params["id"]).Error; err != nil {
+		log.Fatalln(err)
+	}
+
+	// возвращение ответа
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("User deleted successfully")
 }
